a89548818314b8a18521e7771fb0a2d629c51e2d
Daniel Patakfalvi <daniel.patakfalvi@outlook.com>
2026-07-18 13:07:58 +0200

    FirstProject

--- 01FIRSTPROJEKT/FIRSTPROJEKT01.py
+++ 01FIRSTPROJEKT/FIRSTPROJEKT01.py
@@ -1,12 +1,17 @@
 import random
 
 divisible_by_3 = []
+
 scores = [random.randint(1, 1000) for _ in range(100)]
+
 #The program generates 100 random integers between 1 and 1000 and stores them in a list called scores.
+
 for x in scores:
     if x % 3 == 0:
         divisible_by_3.append(x)
+
 print(divisible_by_3)
+
 """
 The programm checks each number in the scores list to see if it is divisible by 3.
 If a number is divisible by 3, it is added to the divisible_by_3 list.
